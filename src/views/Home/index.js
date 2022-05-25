4b283fb892eef1f23e9c40ac41da111977fe19ab
klissman <granadosklissman@gmail.com>
2022-05-25 15:10:14 -0400

    outlet

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
 export default function Home() {
     return (
         <>
             <p>
                 Agrupación de componentes relacionados con tutoriales de react.
             </p>
+            <Outlet />
         </>
     );
 }
